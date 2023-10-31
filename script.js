b01e7290699785907fd9eb9fa0eeb39983f99b32
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-10-31 18:56:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function drawCircle() {
         // Draw the circle
         ctx.beginPath();
         ctx.arc(200, 200, 50, 0, 2 * Math.PI); // Circle with a center at (200, 200) and a radius of 50
-        ctx.fillRect(200, 200, 50, 50);
+        ctx.fillrect(200, 200, 50, 50);
         ctx.fillStyle = '#FF0000'; // Set the fill color to red
         ctx.fill();
         ctx.stroke();
