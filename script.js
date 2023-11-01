da5b310df0acbf2044fb1392684ad66ef28a0d85
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-01 18:54:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var ctx = c.getContext("2d");
 // Display settings
 var minW = 0;
 var minH = 0;
-var maxW = canvas1.width;   //width is 600 pixels
-var maxH = canvas1.height;   //height is 600 pixels
+var maxW = canvas1.width;   //width is 800 pixels
+var maxH = canvas1.height;   //height is 800 pixels
 var bgHue = "#777777";
 
-var pixS = 6;
-var pCX = Math.floor(maxW / pixS);  //100
-var pCY = Math.floor(maxH / pixS);  //100
+var pixS = 10;
+var pCX = Math.floor(maxW / pixS);  //80
+var pCY = Math.floor(maxH / pixS);  //80
 var pA = new Array(pCY);
 
 var elHues = {
@@ -62,45 +62,56 @@ function manual_setup(){
         pA[i] = new Array(pCX).fill('air');
     }
 
-    // Set border elements with blocks
+    // Set border elements with blocks, except for a small opening
     for (var x = 0; x < pCX; x++) {
-        pA[pCY - 1][x] = 'block';
-        pA[0][x] = 'block';
+        if (x < 38 || x > 42) { // Opening between x=38 and x=42
+            pA[0][x] = 'block'; // Top border
+            pA[pCY - 1][x] = 'block'; // Bottom border
+        }
     }
-    for (var y = 0; y < pCY; y++) {
-        pA[y][0] = 'block';
-        pA[y][pCX - 1] = 'block';
+    for (var y = 1; y < pCY - 1; y++) {
+        pA[y][0] = 'block'; // Left border
+        pA[y][pCX - 1] = 'block'; // Right border
     }
 
-    // Create a pattern
-    // A hill of powder
-    for (var y = 30; y < 40; y++) {
-        for (var x = 20; x < 80; x++) {
-            if (x >= 50 - (y - 30) && x <= 50 + (y - 30)) {
-                pA[y][x] = 'powder';
-            }
+    // Draw a smiley face using blocks with eyes closer together
+    // Eyes
+    pA[20][35] = 'block';
+    pA[20][45] = 'block';
+    // Smile
+    for (var x = 30; x <= 50; x++) {
+        pA[50][x] = 'block';
+    }
+    pA[49][29] = 'block';
+    pA[48][28] = 'block';
+    pA[49][51] = 'block';
+    pA[48][52] = 'block';
+
+    // Add a few fire elements above the smiley face which will fall and potentially interact with other elements
+    pA[10][35] = 'fire';
+    pA[10][45] = 'fire';
+
+    // A cloud of powder at the top center
+    for (var y = 1; y < 5; y++) {
+        for (var x = 38; x <= 42; x++) {
+            pA[y][x] = 'powder';
         }
     }
 
-    // A pool of water at the bottom
-    for (var y = 80; y < 90; y++) {
-        for (var x = 25; x < 75; x++) {
+    // A pool of water at the bottom, which will loop to the top through the opening
+    for (var y = 75; y < 80; y++) {
+        for (var x = 38; x <= 42; x++) {
             pA[y][x] = 'water';
         }
     }
 
-    // A cloud of gas at the top
-    for (var y = 10; y < 15; y++) {
-        for (var x = 40; x < 60; x++) {
-            pA[y][x] = 'gas';
-        }
-    }
-
-    // A line of fire above the powder
-    for (var x = 20; x < 80; x++) {
-        pA[29][x] = 'fire';
+    // Add some gas elements at the top that can be ignited by the fire
+    for (var x = 30; x <= 50; x++) {
+        pA[5][x] = 'gas';
     }
 }
+
+
 
 
 
@@ -140,8 +151,8 @@ function tick() {
     for (var y = 1; y < pCY; y++) {
         for(var x = 0; x < pCX; x++) {
             if(pA[y][x] == 'air' && (pA[y - 1][x] == 'powder' || pA[y - 1][x] == 'water')){
-                pA_temp[y][x] = pA_temp[y - 1][x]
-                pA_temp[y - 1][x] = 'air'
+                pA_temp[y][x] = pA_temp[y - 1][x];
+                pA_temp[y - 1][x] = 'air';
             }
         }
 
@@ -150,74 +161,141 @@ function tick() {
     // water settling
     for (var y = 0; y < pCY; y++) {
         for(var x = 1; x < pCX - 1; x++) {
+            if(pA[y][x] != 'water'){
+                continue;   // Skip if the tile is not water
+            }
+            
             if(pA[y][x] == 'water' && pA_temp[y][x] == 'water' && pA[y][x - 1] == 'air' && pA_temp[y][x - 1] == 'air' && Math.random() < .1){
-                pA_temp[y][x] = 'air'
-                pA_temp[y][x - 1] = 'water'
+                pA_temp[y][x] = 'air';
+                pA_temp[y][x - 1] = 'water';
             }
             if(pA[y][x] == 'water' && pA_temp[y][x] == 'water' && pA[y][x + 1] == 'air' && pA_temp[y][x + 1] == 'air' && Math.random() < .1){
-                pA_temp[y][x] = 'air'
-                pA_temp[y][x + 1] = 'water'
+                pA_temp[y][x] = 'air';
+                pA_temp[y][x + 1] = 'water';
             }
         }
     }
 
-    // gas moving
+    // Gas moving
     for (var y = 0; y < pCY; y++) {
-        for(var x = 1; x < pCX - 1; x++) {
-            if(pA[y][x] == 'gas' && pA_temp[y][x] == 'gas' && pA[y][x - 1] == 'air' && pA_temp[y][x - 1] == 'air' && Math.random() < .1){
-                pA_temp[y][x] = 'air'
-                pA_temp[y][x - 1] = 'gas'
+        for (var x = 0; x < pCX; x++) {
+            if(pA[y][x] != 'gas'){
+                continue;   // Skip if the tile is not gas
             }
-            if(pA[y][x] == 'gas' && pA_temp[y][x] == 'gas' && pA[y][x + 1] == 'air' && pA_temp[y][x + 1] == 'air' && Math.random() < .1){
-                pA_temp[y][x] = 'air'
-                pA_temp[y][x + 1] = 'gas'
+            
+            var r1 = Math.random();
+            if (r1 > 0.1) {
+                continue; // Skip the current iteration with 90% chance
             }
-            if(pA[y][x] == 'gas' && pA_temp[y][x] == 'gas' && pA[y - 1][x] == 'air' && pA_temp[y - 1][x] == 'air' && Math.random() < .1){
-                pA_temp[y][x] = 'air'
-                pA_temp[y - 1][x] = 'gas'
+
+            // Randomly select a direction
+            var r2 = Math.random();
+            var direction = 'none';
+            if (r2 < .25 && x > 0 && pA_temp[y][x - 1] == 'air') {
+                direction = 'left';
+            } else if (r2 >= .25 && r2 < .5 && x < pCX - 1 && pA_temp[y][x + 1] == 'air') {
+                direction = 'right';
+            } else if (r2 >= .5 && r2 < .75 && y > 0 && pA_temp[y - 1][x] == 'air') {
+                direction = 'up';
+            } else if (r2 >= .75 && y < pCY - 1 && pA_temp[y + 1][x] == 'air') {
+                direction = 'down';
             }
-            if(pA[y][x] == 'gas' && pA_temp[y][x] == 'gas' && pA[y + 1][x] == 'air' && pA_temp[y + 1][x] == 'air' && Math.random() < .1){
-                pA_temp[y][x] = 'air'
-                pA_temp[y + 1][x] = 'gas'
+
+            // Move gas based on the selected direction
+            if (pA[y][x] == 'gas' && pA_temp[y][x] == 'gas') {
+                switch (direction) {
+                    case 'left':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y][x - 1] = 'gas';
+                        break;
+                    case 'right':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y][x + 1] = 'gas';
+                        break;
+                    case 'up':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y - 1][x] = 'gas';
+                        break;
+                    case 'down':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y + 1][x] = 'gas';
+                        break;
+                }
             }
         }
     }
+
+
 
     // fire moving
     for (var y = 0; y < pCY; y++) {
-        for(var x = 1; x < pCX - 1; x++) {
-            if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && pA[y][x - 1] == 'air' && pA_temp[y][x - 1] == 'air' && Math.random() < .05){
-                pA_temp[y][x] = 'air'
-                pA_temp[y][x - 1] = 'fire'
+        for (var x = 0; x < pCX; x++) {
+            
+            if(pA[y][x] != 'fire'){
+                continue;   // Skip the current iteration if the tile is not fire
             }
-            if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && pA[y][x + 1] == 'air' && pA_temp[y][x + 1] == 'air' && Math.random() < .05){
-                pA_temp[y][x] = 'air'
-                pA_temp[y][x + 1] = 'fire'
+
+            var r1 = Math.random();
+            if (r1 > 0.2) {
+                continue; // Skip the current iteration with 80% chance
             }
-            if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && pA[y - 1][x] == 'air' && pA_temp[y - 1][x] == 'air' && Math.random() < .02){
-                pA_temp[y][x] = 'air'
-                pA_temp[y - 1][x] = 'fire'
+            if(r1 < 0.007) {
+                pA_temp[y][x] = 'air'; // Chance of flame dying out
             }
-            if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && Math.random() < 0.003){
-                pA_temp[y][x] = 'air'
+
+            // Randomly select a direction
+            var r2 = Math.random() * 0.9;
+            var direction = 'none';
+            if (r2 < .25 && x > 0 && pA_temp[y][x - 1] == 'air') {
+                direction = 'left';
+            } else if (r2 >= .25 && r2 < .5 && x < pCX - 1 && pA_temp[y][x + 1] == 'air') {
+                direction = 'right';
+            } else if (r2 >= .5 && r2 < .9 && y > 0 && pA_temp[y - 1][x] == 'air') {
+                direction = 'up';
+            }
+
+            // Move gas based on the selected direction
+            if (pA[y][x] == 'fire' && pA_temp[y][x] == 'fire') {
+                switch (direction) {
+                    case 'left':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y][x - 1] = 'fire';
+                        break;
+                    case 'right':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y][x + 1] = 'fire';
+                        break;
+                    case 'up':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y - 1][x] = 'fire';
+                        break;
+                    case 'down':
+                        pA_temp[y][x] = 'air';
+                        pA_temp[y + 1][x] = 'fire';
+                        break;
+                }
             }
         }
     }
 
     // fire overtaking gas or powder
-    for (var y = 0; y < pCY; y++) {
+    for (var y = 1; y < pCY - 1; y++) {
         for(var x = 1; x < pCX - 1; x++) {
+            if(pA[y][x] != 'fire'){
+                continue;   // Skip if the tile is not fire
+            }
+            
             if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && (pA[y][x - 1] == 'gas' && pA_temp[y][x - 1] == 'gas' && Math.random() < .9 || pA[y][x - 1] == 'powder' && pA_temp[y][x - 1] == 'powder' && Math.random() < .03)){
-                pA_temp[y][x - 1] = 'fire'
+                pA_temp[y][x - 1] = 'fire';
             }
             if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && (pA[y][x + 1] == 'gas' && pA_temp[y][x + 1] == 'gas' && Math.random() < .9 || pA[y][x + 1] == 'powder' && pA_temp[y][x + 1] == 'powder' && Math.random() < .03)){
-                pA_temp[y][x + 1] = 'fire'
+                pA_temp[y][x + 1] = 'fire';
             }
             if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && (pA[y - 1][x] == 'gas' && pA_temp[y - 1][x] == 'gas' && Math.random() < .9 || pA[y - 1][x] == 'powder' && pA_temp[y - 1][x] == 'powder' && Math.random() < .03)){
-                pA_temp[y - 1][x] = 'fire'
+                pA_temp[y - 1][x] = 'fire';
             }
             if(pA[y][x] == 'fire' && pA_temp[y][x] == 'fire' && (pA[y + 1][x] == 'gas' && pA_temp[y + 1][x] == 'gas' && Math.random() < .9 || pA[y + 1][x] == 'powder' && pA_temp[y + 1][x] == 'powder' && Math.random() < .03)){
-                pA_temp[y + 1][x] = 'fire'
+                pA_temp[y + 1][x] = 'fire';
             }
         }
     }
@@ -226,8 +304,8 @@ function tick() {
     // allow elements to move through the bottom to the top
     for(var x = 0; x < pCX; x++) {
         if((pA[pCY - 1][x] == 'water' || pA[pCY - 1][x] == 'powder' ) && pA[0][x] == 'air'){
-            pA_temp[0][x] = pA[pCY - 1][x]
-            pA_temp[pCY - 1][x] = 'air'
+            pA_temp[0][x] = pA[pCY - 1][x];
+            pA_temp[pCY - 1][x] = 'air';
         }
 
     }
@@ -235,7 +313,7 @@ function tick() {
     // Rest of the tick function
 
     
-    make_gas_and_fire()
+    make_gas_and_fire();
     
     
     // Assign the updated temporary array back to pA
@@ -287,18 +365,18 @@ function drawElementAtMouse(event) {
 }
 
 function set_element(element){
-    penE = element
+    penE = element;
 }
 
 function make_gas_and_fire(){
     for (var y = 1; y < pCY - 1; y++) {
         for(var x = 1; x < pCX - 1; x++) {
-            if(pA[y][x] == 'air' && pA_temp[y][x] == 'air'){
+            if(pA[y][x] == 'air'){
                 if(Math.random() < 0.00001){
-                    pA_temp[y][x] = 'gas'
+                    pA_temp[y][x] = 'gas';
                 }
                 if(Math.random() < 0.00000001){
-                    pA_temp[y][x] = 'fire'
+                    pA_temp[y][x] = 'fire';
                 }
             }
             
